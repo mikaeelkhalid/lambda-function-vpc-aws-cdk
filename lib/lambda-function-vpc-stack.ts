d5472aa36085577190bc4f743488b19c0d2c9a60
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2024-06-27 21:43:11 +0200

    add vpc and lambda function

--- lib/lambda-function-vpc-stack.ts
+++ lib/lambda-function-vpc-stack.ts
@@ -1,8 +1,42 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { IpAddresses, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Runtime, Function, Code} from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
 export class LambdaFunctionVpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const vpc = new Vpc(this, 'my-vpc', {
+      ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+      natGateways: 1,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'private-subnet-1',
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'public-subnet-1',
+          subnetType: SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+      ],
+    });
+
+    const lambdaFunction = new Function(this, 'my-lambda-function', {
+      runtime: Runtime.NODEJS_18_X,
+      vpc,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      memorySize: 1024,
+      timeout: Duration.seconds(5),
+      handler: '',
+      code: Code.fromAsset(join(__dirname, '')),
+    });
+
   }
 }
