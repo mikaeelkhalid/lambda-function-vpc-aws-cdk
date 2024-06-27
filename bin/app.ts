35078965ea27ca3e2bb4b8d45a8e6e2deb6900a1
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2024-06-27 22:05:08 +0200

    update starter

--- bin/app.ts
+++ bin/app.ts
@@ -4,4 +4,11 @@ import { App } from 'aws-cdk-lib';
 import { LambdaFunctionVpcStack } from '../lib/lambda-function-vpc-stack';
 
 const app = new App();
-new LambdaFunctionVpcStack(app, 'LambdaFunctionVpcStack', {});
+
+new LambdaFunctionVpcStack(app, 'lambda-function-vpc-stack', {
+    stackName: 'lambda-function-vpc-stack',
+    env: {
+        account: 'xxxxxxxxx', // change before deploy
+        region: 'xxxxxxxxx'  // change before deploy
+    }
+});
